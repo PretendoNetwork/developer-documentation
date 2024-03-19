a60dcdd7fdea0ba76722e4679f0b867f490790c1
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-03-19 15:02:16 -0400

    overview: update root sidebar nex-go link

--- sites/overview.tsx
+++ sites/overview.tsx
@@ -14,7 +14,7 @@ export default site('overview', {
 				group('See also', [
 					link('PRUDP', '/overview/prudp'),
 					link('RMC', '/overview/rmc'),
-					link('nex-go', '/overview/nex-go'),
+					link('nex-go', '/docs/nex-go'),
 					link('nex-protocols-go', '/docs/nex-protocols-go'),
 					link('nex-protocols-common-go', '/docs/nex-protocols-common-go')
 				])
